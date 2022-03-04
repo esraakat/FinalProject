9b6f2fde08694957b2e753b2fcda6d841031828c
Esra Akat <383196@ogr.ktu.edu.tr>
2022-03-04 16:20:44 +0300

    4.3.2022

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,16 +1,20 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using System;
 
 namespace ConsoleUI
 {
+    //SOLID
+    //Open Closed Principle - Yaptığın yazılıma yeni bir özellik ekliyorsan mevuttaki hiçbir koduna dokunamazsın
     class Program
     {
         static void Main()
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
 
-            foreach (var product in productManager.GetAll())
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetByUnitPrice(50,100))
             {
                 Console.WriteLine(product.ProductName);
             }
