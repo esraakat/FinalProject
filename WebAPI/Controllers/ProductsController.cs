37384a7af6d218feefce653adb0f36894d28d1df
Esra Akat <383196@ogr.ktu.edu.tr>
2022-03-24 21:27:55 +0300

    WebAPI katmanı eklendi.

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")] //bu isteği yaparken insanlar bize nasıl ulaşssın? başına api yazsın
+    [ApiController] //attribute
+    public class ProductsController : ControllerBase
+    {
+        //naming convention
+        //IoC Container - Inversion of Control - Değişimin kontrolü - 
+        IProductService _productService; //field'ların defaultu private
+        public ProductsController(IProductService productService) //loose coupling - gevşek bağlılık
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public List<Product> Get()
+        {
+            //dependancy chain
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+    }
+}
+//business'daki datayı burada döndürebiliriz.
