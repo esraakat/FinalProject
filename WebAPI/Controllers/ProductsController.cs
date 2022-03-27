d5ba31d8cb6001b89e3b5bc7173def17860c649d
Esra Akat <383196@ogr.ktu.edu.tr>
2022-03-27 19:29:52 +0300

    WebAPI katmanı eklendi.

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -1,30 +1,54 @@
 ﻿using Business.Abstract;
-using Business.Concrete;
-using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace WebAPI.Controllers
 {
-    [Route("api/[controller]")] //bu isteği yaparken insanlar bize nasıl ulaşssın? başına api yazsın
-    [ApiController] //attribute
+    [Route("api/[controller]")]
+    [ApiController]
     public class ProductsController : ControllerBase
     {
-        //naming convention
-        //IoC Container - Inversion of Control - Değişimin kontrolü - 
-        IProductService _productService; //field'ların defaultu private
-        public ProductsController(IProductService productService) //loose coupling - gevşek bağlılık
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
         {
             _productService = productService;
         }
-        [HttpGet]
-        public List<Product> Get()
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
         {
-            //dependancy chain
             var result = _productService.GetAll();
-            return result.Data;
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")] //data ekleme
+        //Post requestlerde 
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
         }
     }
 }
-//business'daki datayı burada döndürebiliriz.
